ad4c2652678e2235109c3cceb11f4c1ae9072b8f
Stefan Hagen <stefan@hagen.link>
2020-07-19 19:08:14 +0200

    Added a negative prefix compression test using set instead of list without policy alongside the prefix compression package documentation

--- tests/test_purge_range.py
+++ tests/test_purge_range.py
@@ -65,3 +65,10 @@ def test_prefix_compression_documentation_ok_tuple_no_policy_example():
     sequence = ("bar/baz", "bar/bazaar")
     expect = ("bar/baz", ["", "aar"])
     assert pr.prefix_compression(sequence, policy=None) == expect
+
+
+def test_prefix_compression_documentation_nok_set_no_policy_example():
+    sequence = {"bar/baz", "bar/bazaar"}
+    message = r"'set' object is not subscriptable"
+    with pytest.raises(TypeError, match=message):
+        pr.prefix_compression(sequence, policy=None)

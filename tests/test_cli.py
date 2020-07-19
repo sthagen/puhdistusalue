6e0d5edeab01c2cd620e9f7eed247a58b44e3f6d
Stefan Hagen <stefan@hagen.link>
2020-07-19 18:36:52 +0200

    Added negative cli test case for integer argument

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -7,3 +7,9 @@ import purge_range.cli as cli
 
 def test_main_nok_empty():
     assert cli.main([]) is None
+
+
+def test_main_nok_int():
+    message = r"argument of type 'int' is not iterable"
+    with pytest.raises(TypeError, match=message):
+        cli.main(42)

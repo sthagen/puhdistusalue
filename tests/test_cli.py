c5a272da895e068fe90af1f9583e39dce175185c
Stefan Hagen <stefan@hagen.link>
2020-07-19 18:40:49 +0200

    Added safeguard assertion to test in case a folder with name 1 exists in parent folder of tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 # pylint: disable=missing-docstring,unused-import,reimported
+import pathlib
 import pytest  # type: ignore
 
 import purge_range.cli as cli
@@ -17,6 +18,7 @@ def test_main_nok_int():
 
 def test_main_nok_ints():
     sequence_of_ints = [1, 2, 3]
+    assert pathlib.Path(str(sequence_of_ints[0])).is_dir() is False, "Unexpected folder 1 exists which breaks this test"
     message = f"\\[Errno 20\\] Not a directory: {sequence_of_ints[0]}"
     with pytest.raises(NotADirectoryError, match=message):
         cli.main(sequence_of_ints)

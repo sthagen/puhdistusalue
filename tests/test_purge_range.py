ec0d877f2f64252e640ace8572a9bce80601ae87
Stefan Hagen <stefan@hagen.link>
2020-07-19 19:00:30 +0200

    Added another single string test with repetitive letters

--- tests/test_purge_range.py
+++ tests/test_purge_range.py
@@ -12,3 +12,8 @@ def test_triage_hashes_ok_empty_hash_map():
 def test_prefix_compression_ok_string():
     texts = "imension is implicit"
     assert pr.prefix_compression(texts) == (texts, [""])
+
+
+def test_purge_range_ok_same_chars_in_string():
+    texts = "a a a a"
+    assert pr.prefix_compression(texts) == (texts, [""])

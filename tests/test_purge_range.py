63b428f3fa0113a4b193660539593e11360c30da
Stefan Hagen <stefan@hagen.link>
2020-07-19 18:53:21 +0200

    Added positive test case for purge range prefix compression on single string - which fails because of the outdated stale copy of the function

--- tests/test_purge_range.py
+++ tests/test_purge_range.py
@@ -7,3 +7,8 @@ import purge_range.purge_range as pr
 
 def test_triage_hashes_ok_empty_hash_map():
     assert pr.triage_hashes({}) == ([], [])
+
+
+def test_prefix_compression_ok_string():
+    texts = "imension is implicit"
+    assert pr.prefix_compression(texts) == (texts, [""])

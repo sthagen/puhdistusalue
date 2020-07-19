db6af8caa8f90450d40b83d32142b87abb05bb25
Stefan Hagen <stefan@hagen.link>
2020-07-19 18:51:22 +0200

    Added positive test case for purge range traige hashes function with empty hash map as input

---
+++ tests/test_purge_range.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=missing-docstring,unused-import,reimported
+import pytest  # type: ignore
+
+import purge_range.purge_range as pr
+
+
+def test_triage_hashes_ok_empty_hash_map():
+    assert pr.triage_hashes({}) == ([], [])

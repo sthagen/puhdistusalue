5588aa850c15ac21d7cb934eebe5b0dcaef8f897
Stefan Hagen <stefan@hagen.link>
2022-12-26 23:12:06 +0100

    Added boring version implementation hook

--- puhdistusalue/__init__.py
+++ puhdistusalue/__init__.py
@@ -1,3 +1,8 @@
 # -*- coding: utf-8 -*-
 """Puhdistusalue (Finnish for clean area here meaning purge range)."""
-__version__ = '2022.7.24'
+# [[[fill git_describe()]]]
+__version__ = '2022.12.13+parent.ff7049ef'
+# [[[end]]] (checksum: 853a9f21e62834d23aa0601ad62a61ce)
+__version_info__ = tuple(
+    e if '-' not in e else e.split('-')[0] for part in __version__.split('+') for e in part.split('.') if e != 'parent'
+)

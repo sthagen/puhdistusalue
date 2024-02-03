761fe8600508b580c608b55b835399eb27e6e7ab
Stefan Hagen <stefan@hagen.link>
2024-02-03 17:48:33 +0100

    Happy linter, warmer winter
    Signed-off-by: Stefan Hagen <stefan@hagen.link>

--- puhdistusalue/__init__.py
+++ puhdistusalue/__init__.py
@@ -1,4 +1,5 @@
 """Puhdistusalue (Finnish for clean area here meaning purge range)."""
+
 # [[[fill git_describe()]]]
 __version__ = '2022.12.13+parent.ff7049ef'
 # [[[end]]] (checksum: 853a9f21e62834d23aa0601ad62a61ce)
